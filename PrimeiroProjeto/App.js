68cba1656c564c3497efb158f6ddbd3d8889fe75
Leonardo Leite <leonardotleite13k@gmail.com>
2018-10-09 00:58:46 -0300

    Finalizado app mimimi

--- PrimeiroProjeto/App.js
+++ PrimeiroProjeto/App.js
@@ -12,7 +12,13 @@ export default class PrimeiroProjeto extends Component {
 
 	mudarVogais(texto) {
 		let novoTexto = texto.toLowerCase();
-		
+		novoTexto = novoTexto.replace(/(a|e|i|o|u)/g, 'i');
+		novoTexto = novoTexto.replace(/(á|à|ã|â)/g, 'i');
+		novoTexto = novoTexto.replace(/(é|è|ê)/g, 'i');
+		novoTexto = novoTexto.replace(/(í|ì|î)/g, 'i');
+		novoTexto = novoTexto.replace(/(ó|ò|ô)/g, 'i');
+		novoTexto = novoTexto.replace(/(ú|ù|û)/g, 'i');
+
 		return novoTexto;
 	}
 
@@ -83,7 +89,7 @@ const styles = StyleSheet.create ({
 
 	},
 	texto:{
-		fontSize:25,
+		fontSize:20,
 		color:'#FFFFFF',
 		padding:10,
 		backgroundColor:'transparent',
