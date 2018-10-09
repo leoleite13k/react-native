c7b1120eb1fa134599c60056ca911c2b7440f3c2
Leonardo Leite <leonardotleite13k@gmail.com>
2018-10-09 00:17:55 -0300

    Criando primiero projeto

--- PrimeiroProjeto/App.js
+++ PrimeiroProjeto/App.js
@@ -1,14 +1,103 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import { View, Text, StyleSheet, Button, TextInput, Image } from 'react-native';
 
 export default class PrimeiroProjeto extends Component {
+	
+	constructor(props){
+		super(props);
+		this.state = {texto1:'Texto 1', texto2:'Texto 2'};
 
-  render() {
+		this.escrever = this.escrever.bind(this);
+	}
 
-    return(
-      <View style= {{flex:1, backgroundColor:'black'}}>
+	mudarVogais(texto) {
+		let novoTexto = texto.toLowerCase();
+		
+		return novoTexto;
+	}
 
-      </View> 
-    );
-  }
+	escrever(t) {
+		let s = this.state;
+		s.texto1 = t;
+		s.texto2 = this.mudarVogais(t);
+
+		this.setState(s);
+	}
+
+	render() {
+
+		return(
+			<View style={styles.body}>
+				<View> 
+					<Text style={styles.titulo}>Criador de mimimi </Text>
+				</View>
+
+				<View style={styles.inputArea}>
+					<TextInput style={styles.input} onChangeText={this.escrever}/>
+				</View>
+
+				<View style={styles.area}>
+					<Text  style={[styles.texto, styles.texto1]}>{this.state.texto1.toUpperCase()}</Text>
+					<Image style={styles.guri} source={require('./images/mimimi.jpg')}/>
+					<Text  style={[styles.texto, styles.texto2]}>{this.state.texto2.toUpperCase()}</Text>
+				</View>
+			</View>
+		);
+	}
 }
+
+const styles = StyleSheet.create ({
+	body:{
+		backgroundColor:'#999999',
+		paddingTop:30,
+		flex:1,
+		flexDirection:'column',
+		alignItems:'center'
+	},
+	titulo:{
+		fontSize:30,
+		color:'#FFFFFF'
+	},
+	inputArea:{
+		alignSelf:'stretch'
+	},
+	input:{
+		borderWidth:1,
+		borderColor:'#999999',
+		backgroundColor:'#EEEEEE',
+		color:'#000000',
+		height:40,
+		margin:20,
+		padding:10
+	},
+	area:{
+		width:300,
+		height:300,
+		marginTop:10
+	},
+	guri:{
+		width:300,
+		height:300,
+		marginTop:-70,
+		zIndex:0
+
+	},
+	texto:{
+		fontSize:25,
+		color:'#FFFFFF',
+		padding:10,
+		backgroundColor:'transparent',
+		fontWeight:'bold',
+		textAlign:'center',
+		height:70
+	},
+	texto1:{
+		zIndex:1
+
+	},
+	texto2:{
+		zIndex:1,
+		marginTop:-70
+
+	}
+});
