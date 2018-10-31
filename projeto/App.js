958d9d88dd42d3c3d67ddc9e26c5dc6c5c9c7797
Leonardo Leite <leonardotleite13k@gmail.com>
2018-10-31 13:18:37 -0300

    Voltando Bonieky

---
+++ projeto/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { View, Text, Button, StyleSheet} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class Tela1 extends Component {
+    static navigationOptions = {
+        title:"Tela 1"
+    }
+
+    render() {
+        return(
+            <View>
+                <Text>Pagina Main</Text>
+                <Button title="Ir para Tela 2" onPress={() => this.props.navigation.navigate('Tela2')} />
+            </View>
+        );
+    }
+}
+
+class Tela2 extends Component {
+    static navigationOptions = {
+        title:"Tela 2"
+    }   
+
+    render() {
+        return (
+            <View>
+                <Text>Pagina 2</Text>
+                <Button title="Voltar" onPress={() => this.props.navigation.goBack()} />
+            </View>    
+        );
+    }
+}
