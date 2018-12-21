f654a648c8bd74450ade987a9e167fcc08b5585d
Leonardo Leite <leonardoleite@MacBook-Air-de-Leonardo.local>
2018-12-21 00:16:57 -0200

    Realizado a 2/4 do App To-Do

--- ToDo/App.js
+++ ToDo/App.js
@@ -5,79 +5,100 @@ import ListaItem from './src/ListaItem';
 
 export default class App extends Component{
 
-  constructor(props) {
-    super(props)
+    constructor(props) {
+        super(props)
   
-    this.state = {
-       lista:[],
-       input:''
-    };
+        this.state = {
+           lista:[],
+           input:''
+        };
 
-    fetch('https://b7web.com.br/todo/42272')
-      .then((retorno) => retorno.json())
-      .then((json) => {
+        this.url = 'https://b7web.com.br/todo/42272';
+
+        this.loadLista();
+
+        this.loadLista = this.loadLista.bind(this);
+        this.addButton = this.addButton.bind(this);
+    }
+
+    loadLista() {
+        fetch(this.url)
+            .then((retorno) => retorno.json())
+            .then((json) => {
+                let state = this.state;
+                state.lista = json.todo;
+                this.setState(state);
+            })
+    }
+
+    addButton() {
+        let texto = this.state.input;
+
         let state = this.state;
-        state.lista = json.todo;
+        state.input = '';
         this.setState(state);
-      })
 
-      this.addButton = this.addButton.bind(this);
-  }
+        fetch(this.url, {
+            method:'POST',
+            headers:{
+                'Accept':'application/json',
+                'Content-Type':'application/json'
+            },
+            body:JSON.stringify({
+                item:texto
+            })
+        })
+            .then((retorno) => retorno.json())
+            .then((json) => {
+                alert("Item inserido com sucesso!")
+                this.loadLista();
+            })
+    }
 
-  addButton() {
-    let texto = this.state.input;
-
-    let state = this.state;
-    state.input = '';
-    this.setState(state);
-
-    
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.addArea}>
-          <Text style={styles.addTxt}>Acione uma nova tarefa</Text>
-          <TextInput style={styles.input} onChangeText={(texto) => {
-            let state = this.state;
-            state.input = texto;
-            this.setState(state);
-          }} value={this.state.input} />
-          <Button title="Adicionar" onPress={this.addButton}/>
-        </View>
-        <FlatList 
-          data={this.state.lista}
-          renderItem={({item}) => <ListaItem data={item} />}
-          keyExtractor={(item, index) => item.id}
-        />
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.addArea}>
+                    <Text style={styles.addTxt}>Acione uma nova tarefa</Text>
+                    <TextInput style={styles.input} onChangeText={(text) => {
+                            let state = this.state;
+                            state.input = text;
+                            this.setState(state);
+                        }} value={this.state.input} />
+                    <Button title="Adicionar" onPress={this.addButton}/>
+                </View>
+                <FlatList 
+                    data={this.state.lista}
+                    renderItem={({item}) => <ListaItem data={item} />}
+                    keyExtractor={(item, index) => item.id}
+                />
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: Platform.OS == 'ios' ? 30 : 0
-  },
-  addArea:{
-    marginBottom: 20,
-    backgroundColor: '#DDDDDD',
-    height: 100
-  },
-  input:{
-    height: 40,
-    backgroundColor: '#CCCCCC',
-    marginLeft: 20,
-    marginRight: 20,
-    paddingLeft: 10,
-    paddingRight: 10
-  },
-  addTxt:{
-    fontSize: 14,
-    textAlign: 'center',
-    marginBottom: 10,
-    marginTop: 10
-  }
+    container: {
+        flex: 1,
+        marginTop: Platform.OS == 'ios' ? 30 : 0
+    },
+        addArea:{
+        marginBottom: 20,
+        backgroundColor: '#DDDDDD',
+        height: 120
+    },
+        input:{
+        height: 40,
+        backgroundColor: '#CCCCCC',
+        marginLeft: 20,
+        marginRight: 20,
+        paddingLeft: 10,
+        paddingRight: 10
+    },
+    addTxt:{
+        fontSize: 14,
+        textAlign: 'center',
+        marginBottom: 10,
+        marginTop: 10
+    }
 });
